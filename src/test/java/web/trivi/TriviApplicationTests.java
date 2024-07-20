d6e925ee025180386aea414593aa268cc9b0ba78
jkjkh1318 <jkjkh1318@gmail.com>
2024-07-20 22:41:24 +0900

    feat: 최초 프로젝트 환경설정(gradle 등)

---
+++ src/test/java/web/trivi/TriviApplicationTests.java
@@ -0,0 +1,13 @@
+package web.trivi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
