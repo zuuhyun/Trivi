22f5c0f5d27b615e151c82b9e9186bff9d4c6463
zuuhyun <lokiki0319@gmail.com>
2024-07-23 16:46:21 +0900

    feat: user 테이블 및 repository 설정
    (cherry picked from commit 8011fca26ddc52649f3605d8922059fdf9f2c65c)

--- src/main/java/web/trivi/repository/UserRepository.java
+++ src/main/java/web/trivi/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package web.trivi.repository;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import web.trivi.domain.User;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
