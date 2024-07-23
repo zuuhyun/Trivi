88f69ac3718ae143acfb53b3ae33340daf1515e9
zuuhyun <lokiki0319@gmail.com>
2024-07-23 22:13:37 +0900

    Revert "feat: user 테이블 및 repository 설정"
    This reverts commit 22f5c0f5d27b615e151c82b9e9186bff9d4c6463.

--- src/main/java/web/trivi/repository/UserRepository.java
+++ src/main/java/web/trivi/repository/UserRepository.java
@@ -1,9 +1,4 @@
 package web.trivi.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import web.trivi.domain.User;
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
+public interface UserRepository {
 }
