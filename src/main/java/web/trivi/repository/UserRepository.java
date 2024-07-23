10b4b909057130daccf3eb777d4397e9791d9389
zuuhyun <lokiki0319@gmail.com>
2024-07-23 23:23:54 +0900

    feat: domain/User, repository/UserRepository 추가

---
+++ src/main/java/web/trivi/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package web.trivi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import web.trivi.domain.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
