fbe748dc939b1bd6f71d704b5026dddb2be59cd4
zuuhyun <lokiki0319@gmail.com>
2024-07-26 02:06:15 +0900

    feat: aws 버킷에img 업로드 기능 추가
    (cherry picked from commit 118316e0229a8d69c203ad29a04f8cdac3ccd198)

---
+++ src/main/java/web/trivi/controller/AmazonS3Controller.java
@@ -0,0 +1,32 @@
+package web.trivi.controller;
+
+import web.trivi.dto.ApiResponse;
+import web.trivi.service.AwsS3Service;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.http.ResponseEntity;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/s3")
+public class AmazonS3Controller {
+
+    private final AwsS3Service awsS3Service;
+
+    @PostMapping("/image")
+    public ResponseEntity<String> uploadImage(@RequestPart("file") MultipartFile multipartFile) {
+        return ApiResponse.success(awsS3Service.uploadImage(multipartFile));
+    }
+
+    @DeleteMapping("/image")
+    public ResponseEntity<Void> deleteImage(@RequestParam("fileName") String fileName) {
+        awsS3Service.deleteImage(fileName);
+        return ApiResponse.success(null);
+    }
+}
