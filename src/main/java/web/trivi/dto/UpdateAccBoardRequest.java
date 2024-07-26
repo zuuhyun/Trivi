acefd5ba76aba596fd98e66e61a03880a0d7d4fb
zuuhyun <lokiki0319@gmail.com>
2024-07-26 18:50:01 +0900

    feat: 동행찾기 기능 게시판 CRUD 기능 추가      - 게시판 작성: 제목, 작성자, 내용, 모집인원, 국가, 도시, 장소, 미팅시간      - 게시판 수정: 제목, 내용, 장소      - 게시판 조회: 전체 조회, 도시 기준 조회      - 게시판 삭제
    (cherry picked from commit 4c38337a0ba73c34623121670c36b4c4375027f1)

---
+++ src/main/java/web/trivi/dto/UpdateAccBoardRequest.java
@@ -0,0 +1,14 @@
+package web.trivi.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class UpdateAccBoardRequest {
+    private String title;
+    private String content;
+    private String locationName;
+}
