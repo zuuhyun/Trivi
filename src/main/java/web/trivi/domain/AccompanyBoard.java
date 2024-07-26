04fe6396bab34931d0fa0b7fc106fba14fc969a2
zuuhyun <lokiki0319@gmail.com>
2024-07-26 22:12:58 +0900

    fix: 동행찾기 게시판 수정 버그 개선

--- src/main/java/web/trivi/domain/AccompanyBoard.java
+++ src/main/java/web/trivi/domain/AccompanyBoard.java
@@ -12,6 +12,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Builder
 @Getter
+@Setter
 @Entity
 public class AccompanyBoard {
 
@@ -92,8 +93,8 @@ public class AccompanyBoard {
     }
 
     public void update(String title, String content, String locationName){
-        this.title = title;
-        this.content = content;
-        this.locationName = locationName;
+            this.title = title;
+            this.content = content;
+            this.locationName = locationName;
     }
 }
