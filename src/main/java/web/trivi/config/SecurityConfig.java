dbce02cb1d586d8015747d73bffe71e65e0df818
jkjkh1318 <jkjkh1318@gmail.com>
2024-07-27 15:30:53 +0900

    fix: CORS 허용

--- src/main/java/web/trivi/config/SecurityConfig.java
+++ src/main/java/web/trivi/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package web.trivi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,6 +20,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .authorizeRequests()
 //                .requestMatchers("/security-login/info").authenticated()
